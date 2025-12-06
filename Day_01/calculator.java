a3ac5db9510d02823d9144486280bdd4dd94d68c
Arun J <141573722+Arun2895@users.noreply.github.com>
2025-12-06 22:41:53 +0530

    Implement basic calculator operations using switch

--- Day_01/calculator.java
+++ Day_01/calculator.java
@@ -1,1 +1,31 @@
+class calculator {
+    public static void main(String[]args){
+        int a = 100;
+        int b = 200;
+        String operation = "add";
 
+        switch(operation){
+            case "add" -> {
+                System.out.println("Addition: " + (a + b));
+            }
+            case "subtract" -> {
+                System.out.println("Subtraction: " + (a - b));
+            }
+            case "multiply" -> {
+                System.out.println("Multiplication: " + (a * b));
+            }
+            case "divide" -> {
+                System.out.println("Division: " + (a / b));
+            }
+            case "modulus" -> {
+                System.out.println("Modulus: " + (a % b));
+
+            }
+
+            default -> {
+                System.out.println("Invalid Operation");
+
+            }
+            }
+        }
+    }
