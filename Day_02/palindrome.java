1f6a7e450acecf0209d880c50d31c29380eeb7bb
Arun J <141573722+Arun2895@users.noreply.github.com>
2025-12-08 00:04:18 +0530

    Add palindrome check for integer input
    Implement palindrome check for integers.

---
+++ Day_02/palindrome.java
@@ -0,0 +1,24 @@
+//To check whether an integer is palindrome or not
+import java.util.Scanner;
+
+class palindrome{
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int rev = 0;
+    int value = n;
+    if(n==0){
+      System.out.println("The given integer is invalid");
+    }
+    while(n>0){
+      int r = n%10;
+      rev = rev*10 + r;
+      n = n/10;
+    }
+    if(value == rev){
+      System.out.println(value + " and " + rev + " are Palindromes");
+    }else{
+      System.out.println(value + " and " + rev + " are not Palindromes");
+    }
+  }
+}
