49db80e0fc65ece2311274f334a54443e6134363
Arun J <141573722+Arun2895@users.noreply.github.com>
2025-12-09 20:16:21 +0530

    Fix class name from 'palindrome' to 'Palindrome'

--- Day_02/Palindrome.java
+++ Day_02/Palindrome.java
@@ -1,7 +1,7 @@
 //To check whether an integer is palindrome or not
 import java.util.Scanner;
 
-class palindrome{
+class Palindrome{
   public static void main(String[]args){
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
