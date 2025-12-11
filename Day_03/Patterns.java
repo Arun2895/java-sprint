b413dbbb30baff7a6db93fde4237f34c179dc592
Arun J <141573722+Arun2895@users.noreply.github.com>
2025-12-11 20:09:58 +0530

    Update Patterns.java

--- Day_03/Patterns.java
+++ Day_03/Patterns.java
@@ -26,3 +26,41 @@ class Patterns{
         }
     }
 }
+
+//Decreasing repeated number pattern
+class Patterns{
+    public static void main(String[] args) {
+        int n = 5;
+        int var1 = 5;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=var1;j++){
+                System.out.print(j);
+                }
+                System.out.println();
+            var1--;
+        }
+    }
+}
+
+
+//Decreasing repeated number pattern
+import java.util.Scanner;
+class Patterns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int repeat = n;
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= repeat; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+            repeat--;
+        }
+
+        sc.close();
+    }
+}
+
+
