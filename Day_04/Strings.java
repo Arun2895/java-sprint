7b93c03f1c9089edf10deff02166e6b347b77af1
Arun J <141573722+Arun2895@users.noreply.github.com>
2025-12-17 22:38:17 +0530

    Update Strings.java

--- Day_04/Strings.java
+++ Day_04/Strings.java
@@ -23,3 +23,22 @@ class Strings{
     }
     }
 }
+
+    import java.util.Scanner;
+    // Frequency check of strings
+    class Strings{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        char s = sc.next().charAt(0);
+        StringBuilder s1 = new StringBuilder("banana");
+        int n = s1.length();
+        int sum = 0;
+        for(int i = 0;i<n;i++){
+            char ch=s1.charAt(i);
+            if(s == ch){
+                sum +=1;
+            }
+        }
+        System.out.println(sum);
+    }
+    }
