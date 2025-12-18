d0a64cf8416a61146e795171d2290af10f16a85c
Arun J <141573722+Arun2895@users.noreply.github.com>
2025-12-18 23:38:47 +0530

    Create Superkeyword.java

---
+++ Day_05/Superkeyword.java
@@ -0,0 +1,35 @@
+// PARENT / SUPER CLASS
+class Computer {
+
+    public void portability() {
+        System.out.println("Computer is not portable");
+    }
+
+}
+
+// CHILD / SUB CLASS (inherits Computer)
+class Laptop extends Computer {
+
+    //Method Overriding
+    public void portability() {
+        super.portability();
+        System.out.println("Laptop is portable");
+    }
+}
+
+// CHILD / SUB CLASS (inherits Computer)
+class Mobile extends Computer{
+
+    public void portability(){
+        System.out.println("Handheld");
+    }
+}
+
+class Superkeyword{
+    public static void main(String[] args) {
+
+        Laptop L1 = new Laptop();
+        L1.portability();
+
+    }
+}
